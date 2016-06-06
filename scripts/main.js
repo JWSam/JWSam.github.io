09cb3e96344d3f924f0842c4bd6adee1cc6cff49
JWSam <up703289@myport.ac.uk>
2016-06-06 20:29:06 +0100

    Revert "Upload"
    This reverts commit bc2af93d4e63772f996afd8f0a27f83349f2317e.

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-list = ["Genji", "Reinhardt", "Lúcio", "Pharah", "Mei", "Winston", "Roadhog", "Soldier", "Tracer", "Reaper", "Junkrat", "Widowmaker", "Hanzo", "Zarya", "Symmetra", "Mercy", "McCree", "Bastion", "Torbjörn"]
-
-
-function myFunction() {
-	var random;
-	random = Math.floor((Math.random() * list.length) + 0); 
-	
-    document.getElementById("changeme").innerHTML = list[random];
-}
